ad4e8b2869e2d948f660af6e3f951f3563256cd8
ibilous <ibilous@lohika.com>
2021-10-18 20:38:24 +0300

    Run pretier

--- Week-2/iam.tf
+++ Week-2/iam.tf
@@ -5,9 +5,9 @@ resource "aws_iam_role" "ec2_instance_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Sid       = ""
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
         Principal = {
           Service = "ec2.amazonaws.com"
         }
@@ -18,30 +18,30 @@ resource "aws_iam_role" "ec2_instance_role" {
 
 resource "aws_iam_role_policy" "ec2_iam_role_policy" {
   name = "ec2_iam_role_policy"
-  role = "${aws_iam_role.ec2_instance_role.id}"
+  role = aws_iam_role.ec2_instance_role.id
 
   policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Effect = "Allow"
-                Action = ["s3:ListBucket"]
-                Resource = ["arn:aws:s3:::${var.bucket_name}"]
-            },
-            {
-                Effect = "Allow"
-                Action = [
-                    "s3:*",
-                    "s3:GetObject",
-                    "s3:DeleteObject"
-                ]
-                Resource = ["arn:aws:s3:::${var.bucket_name}/*"]
-            }
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:ListBucket"]
+        Resource = ["arn:aws:s3:::${var.bucket_name}"]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:*",
+          "s3:GetObject",
+          "s3:DeleteObject"
         ]
-    })
+        Resource = ["arn:aws:s3:::${var.bucket_name}/*"]
+      }
+    ]
+  })
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
-    name = "ec2_instance_profile"
-    role = "${aws_iam_role.ec2_instance_role.name}"
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_instance_role.name
 }
--- Week-2/main.tf
+++ Week-2/main.tf
@@ -3,92 +3,92 @@ provider "aws" {
 }
 
 resource "aws_security_group" "ssh_security_group" {
-    name        = "allow_ssh"
-    description = "Allow SSH connection to instance"
+  name        = "allow_ssh"
+  description = "Allow SSH connection to instance"
 
-    ingress = [
-        {
-            description      = "SSH"
-            from_port        = 22
-            to_port          = 22
-            protocol         = "tcp"
-            cidr_blocks      = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            security_groups  = []
-            self             = false      
-        }
-    ]
+  ingress = [
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 
-    egress = [
-        {
-            description      = "SSH"
-            from_port        = 0
-            to_port          = 0
-            protocol         = "-1"
-            cidr_blocks      = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = ["::/0"]
-            prefix_list_ids  = []
-            security_groups  = []
-            self             = false 
-        }
-    ]
+  egress = [
+    {
+      description      = "SSH"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 }
 
 resource "aws_security_group" "http_security_group" {
-    name        = "allow_http"
-    description = "Allow HTTP connection to instance"
+  name        = "allow_http"
+  description = "Allow HTTP connection to instance"
 
-    ingress = [
-        {
-            description      = "HTTP"
-            from_port        = 80
-            to_port          = 80
-            protocol         = "tcp"
-            cidr_blocks      = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            security_groups  = []
-            self             = false      
-        },
-        {
-            description      = "HTTPS"
-            from_port        = 443
-            to_port          = 443
-            protocol         = "tcp"
-            cidr_blocks      = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            security_groups  = []
-            self             = false      
-        }
-    ]
+  ingress = [
+    {
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = "HTTPS"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 
-    egress = [
-        {
-            description      = "HTTP"
-            from_port        = 0
-            to_port          = 0
-            protocol         = "-1"
-            cidr_blocks      = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = ["::/0"]
-            prefix_list_ids  = []
-            security_groups  = []
-            self             = false 
-        }
-    ]
+  egress = [
+    {
+      description      = "HTTP"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 }
 
 resource "aws_instance" "ec2_instance_week_2" {
-    ami                    = var.image_id
-    instance_type          = var.instance_type
-    key_name               = var.key_name
-    vpc_security_group_ids = [
-        "${aws_security_group.http_security_group.id}",
-        "${aws_security_group.ssh_security_group.id}"
-    ]
-    iam_instance_profile   = "${aws_iam_instance_profile.ec2_instance_profile.id}"
-    user_data              = <<EOF
+  ami           = var.image_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [
+    "${aws_security_group.http_security_group.id}",
+    "${aws_security_group.ssh_security_group.id}"
+  ]
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.id
+  user_data            = <<EOF
         #! /bin/bash
         sudo yum update
         aws s3 cp s3://${var.bucket_name}/${var.file_name} /home/ec2-user/${var.file_name}
